b0155c03fbb22f2afc79b90ee71779a01d03609b
DDbrother <derekyin42009@gmail.com>
2025-12-25 19:08:00 -0500

    more basic content + cdn setup
    basic landing content
    
    more scaffolding

---
+++ app/api/v0/page.tsx
@@ -0,0 +1,13 @@
+export default function apiV0() {
+    return (
+        <main className="flex h-screen items-center justify-center bg-black text-white font-sans">
+            <div className="text-center">
+                <h1 className="text-4xl mb-4">ddbrother.me</h1>
+                <p className="text-lg opacity-80">
+                    api docs to come :P
+                </p>
+            </div>
+        </main>
+    );
+}
+
